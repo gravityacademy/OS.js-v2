4cb113e35488c299bd8c14eac225c84c0314e067
tommytee <github@nicecomputersny.com>
2016-02-19 01:52:21 -0700

    guac app

---
+++ src/packages/default/GreenGuac/main.js
@@ -0,0 +1,32 @@
+
+(function(Application, GUI, Dialogs, Utils, API, VFS) {
+  'use strict';
+
+  /////////////////////////////////////////////////////////////////////////////
+  // APPLICATION
+  /////////////////////////////////////////////////////////////////////////////
+
+  function ApplicationGreenGuac(args, metadata) {
+    Application.apply(this, ['ApplicationGreenGuac', args, metadata, {
+      src: '//198.199.101.183:8080/guacamole',
+      title: metadata.name,
+      icon: metadata.icon,
+      width: 640,
+      height: 480,
+      allow_resize: true,
+      allow_restore: true,
+      allow_maximize: true
+    }]);
+  }
+
+  ApplicationGreenGuac.prototype = Object.create(Application.prototype);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  OSjs.Applications = OSjs.Applications || {};
+  OSjs.Applications.ApplicationGreenGuac = OSjs.Applications.ApplicationGreenGuac || {};
+  OSjs.Applications.ApplicationGreenGuac.Class = ApplicationGreenGuac;
+
+})(OSjs.Helpers.IFrameApplication, OSjs.GUI, OSjs.Dialogs, OSjs.Utils, OSjs.API, OSjs.VFS);
